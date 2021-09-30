6a8c0cdf1d9569edbf30639f4e489ef43b19196f
Whitneyto <nguyetto@udel.edu>
2021-09-30 19:27:08 -0400

    Made weather component

--- src/Components/Weather.tsx
+++ src/Components/Weather.tsx
@@ -2,10 +2,16 @@ import { useState, useEffect } from "react"
 
 const APIKey : string = "cb06f13186ff79c17c8d80515448f477";
 export function Weather() : JSX.Element{
-  
-    fetch("https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=cb06f13186ff79c17c8d80515448f477")
+const [secURl, setSecURL] = useState<string>("");
+    fetch("https://api.weather.gov/points/38.9108,-75.5277")
    .then(response => response.json())
-   .then(data => console.log(data))
+   .then(data => {console.log(data.properties.forecast);setSecURL(data.properties.forecast)})
+    
+   fetch(secURl)
+   .then(response => response.json())
+   .then(data2 => console.log(data2.properties.periods[1].temperature));
+
+
    return <div>
    </div>
 }
